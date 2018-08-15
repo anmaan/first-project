943426998f09415b2df5b48008be72dc45e54d40
anmaan <annamaria.th@gmx.at>
2018-08-15 11:33:02 +0200

    initialcommit

---
+++ pages/products/_pid/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>I am a smol product, with (ID: {{ $route.params.pid }}) </h1>
+</template>
+
---
+++ pages/users/_id/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>I am smol, with ID: {{ $route.params.id }} </h1>
+</template>
+
